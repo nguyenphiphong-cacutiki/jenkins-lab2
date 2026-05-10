fae11e3497d50102d3caef120bdd52d2dd915962
tuanvx <tuanvx@goline.com>
2026-05-10 21:32:53 +0700

    udpate agent, remove fail test

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,7 @@
 pipeline {
-    agent any
+    agent {
+        label 'MAVENBUILDER'
+    }
 
     options {
         timeout(time: 10, unit: 'MINUTES')
--- test.js
+++ test.js
@@ -3,5 +3,4 @@ const assert = require('assert');
 
 assert.strictEqual(add(2, 3), 5, 'add(2,3) should be 5');
 assert.strictEqual(greet('World'), 'Hello, World!', 'greet should work');
-assert.strictEqual(add(2, 3), 999, 'intentional fail');
 console.log('All tests passed.');
